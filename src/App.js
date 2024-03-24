515b468522ce63b7d3766197950e2703e26bfa37
shivam1611 <iamshivamsharma755@gmail.com>
2024-03-24 08:20:03 +0530

    Error component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useEffect, useReducer } from "react";
+import "./App.css";
 
-import './App.css';
+import Header from "./Header";
 
 function App() {
+
+
+  const initialState = {
+    questions : [],
+    state : "loading"
+  }
+
+  function reducer(state, action){
+    switch(action.type){
+      case "dataRecieved":
+        return {
+          ...state,
+          questions : action.payload
+        }
+        
+        default:
+          throw new Error("Action Unknown") 
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data)=>dispatch({type:"dataRecieved", payload:data}))
+      .catch((err) => console.log(err));
+  });
   return (
     <div className="App">
-     <p>Hello World</p>
+      <Header/>
+
+
     </div>
   );
 }
